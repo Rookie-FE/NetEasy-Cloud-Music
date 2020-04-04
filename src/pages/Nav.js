12792802fddc7ca8373474b72527bfcb97aef4a2
Alen <290017499@qq.com>
2020-04-04 17:10:57 +0800

    1

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom'
-
-function TopNav(props){
-    return(
+import { withRouter } from 'react-router-dom';
+let x = 1;
+let obj = {
+    sdf: 'dd',
+};
+let my = { dslfsdj: 'dsfsd' };
+function TopNav(props) {
+    return (
         <>
-        <button onClick={() => {
-                props.history.push('/recommend')
-            }}>推荐</button>
-            <button onClick={() => {
-                props.history.push('/singer')
-            }}>歌手</button>
-            <button onClick={() => {
-                props.history.push('/ranking')
-            }}>排行榜</button>
+            <button
+                onClick={() => {
+                    props.history.push('/recommend');
+                }}
+            >
+                推荐
+            </button>
+            <button
+                onClick={() => {
+                    props.history.push('/singer');
+                }}
+            >
+                歌手
+            </button>
+            <button
+                onClick={() => {
+                    props.history.push('/ranking');
+                }}
+            >
+                排行榜
+            </button>
         </>
-    )
+    );
 }
-export default  withRouter(TopNav)
+export default withRouter(TopNav);

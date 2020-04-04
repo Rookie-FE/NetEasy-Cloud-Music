80560f471fe4b285047c079944ef5f3ccda3a5b1
Alen <290017499@qq.com>
2020-04-04 17:54:48 +0800

    interval

--- src/component/Error.js
+++ src/component/Error.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import {Button} from 'antd';
 
 export default function Error(props) {
+    const [times,setTimes] = useState(()=>(3));
+
+    useEffect(()=>{
+        const $timer = setInterval(() => {
+            if (times === 0) {
+                props.history.replace('/recommend')
+            }else {
+                setTimes(a=>a-1)
+            }
+        }, 1000);
+        return ()=>{
+            clearInterval($timer)
+        }
+    },[])
      
     return (
         <div>
             404 Error<br/>
             <Button type='primary' onClick={()=>{
                 props.history.replace('/recommend')
-            }} >跳转到首页</Button>
+            }} >{times}跳转到首页</Button>
         </div>
     )
 }

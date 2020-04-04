789852cb4fbc6adb13304939e76f3ec0f3141a2e
刘焕 <liuhuan247_2014@163.com>
2020-04-04 15:38:04 +0800

    test husky

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+let aaa = 1;
+
 ReactDOM.render(
     <Provider store={store}>
         <div>hello world</div>

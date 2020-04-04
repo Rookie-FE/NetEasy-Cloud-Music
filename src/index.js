f9fb70a063777f99d8d8e66aad4322227f20cdc4
BigBrother-Huan <4485043@qq.com>
2020-04-04 12:47:08 +0800

    test 111

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store'
 
+
+console.log(111);
+
 ReactDOM.render(
   <React.StrictMode>
     <div>hello world</div>

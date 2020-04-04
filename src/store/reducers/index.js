8be8cdbc9138d5149e08f085d22d51630a8c9c88
刘焕 <liuhuan247_2014@163.com>
2020-04-04 15:44:57 +0800

    will blank remove?

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,4 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux';
 
-
-export default combineReducers({
-
-})
+console.log('hello husky!');
+export default combineReducers({});

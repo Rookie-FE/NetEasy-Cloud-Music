05d82b448ef95cb97601f1e23df2b8477e1a7c4e
刘焕 <liuhuan247_2014@163.com>
2020-04-04 15:37:17 +0800

    husky eslint prettier config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './store';
 
-
-console.log(111);
-
 ReactDOM.render(
-  <Provider store={store}>
-    <div>hello world</div>
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <div>hello world</div>
+    </Provider>,
+    document.getElementById('root'),
 );

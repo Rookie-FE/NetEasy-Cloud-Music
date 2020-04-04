edc2b60af875ca0aa747a6a6c611a649398ff422
刘焕 <liuhuan247_2014@163.com>
2020-04-04 16:44:24 +0800

    source tree 提交

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
+console.log('is precommit ok in vscode?'        );
 ReactDOM.render(
     <Provider store={store}>
         <div>hello world</div>

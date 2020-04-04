24d2e48516b7a5751b09c371cc87eab9b83f8113
Alen <290017499@qq.com>
2020-04-04 13:06:58 +0800

    import antd

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const { override, fixBabelImports, addLessLoader, addWebpackPlugin } = require('customize-cra');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+
+module.exports = override(
+    addWebpackPlugin(new AntdDayjsWebpackPlugin()),
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { '@primary-color': '#1DA57A' },
+    }),
+);
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
+import {Button} from 'antd'
 
 ReactDOM.render(
   <Provider store={store}>
-    <div>hello world lllll</div>
+    <div>hello world 
+    <Button type="primary">Button</Button>
+    </div>
   </Provider>,
   document.getElementById('root')
 );

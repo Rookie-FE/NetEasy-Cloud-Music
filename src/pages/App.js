ae883c8365ae92fef6ea6f98edf575278997fc37
刘焕 <liuhuan247_2014@163.com>
2020-04-07 09:35:31 +0800

    delete code

--- src/pages/App.js
+++ src/pages/App.js
@@ -8,16 +8,7 @@ import routes from '../routes';
 export default function App() {
     return (
         <Provider store={store}>
-            <HashRouter>
-                {renderRoutes(routes)}
-                {/* <Switch>
-                    <Redirect from='/' exact to='/recommend' />
-                    <Route path='/recommend' component={Recommend} />
-                    <Route path='/singer' component={Singer} />
-                    <Route path='/ranking' component={Ranking} />
-                    <Route path='*' component={Error} />
-                </Switch> */}
-            </HashRouter>
+            <HashRouter>{renderRoutes(routes)}</HashRouter>
         </Provider>
     );
 }

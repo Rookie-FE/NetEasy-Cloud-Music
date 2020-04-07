24217a87019388700839a3f05b772ef70fff8242
刘焕 <liuhuan247_2014@163.com>
2020-04-07 09:31:15 +0800

    set styles nav

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
 import App from './pages/App';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
-);
-console.log('test1');
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
 import { HashRouter } from 'react-router-dom';
+import store from '../store/index';
 import routes from '../routes';
 
 export default function App() {
     return (
-        <HashRouter>
-            {renderRoutes(routes)}
-            {/* <Switch>
-                <Redirect from='/' exact to='/recommend' />
-                <Route path='/recommend' component={Recommend} />
-                <Route path='/singer' component={Singer} />
-                <Route path='/ranking' component={Ranking} />
-                <Route path='*' component={Error} />
-            </Switch> */}
-        </HashRouter>
+        <Provider store={store}>
+            <HashRouter>
+                {renderRoutes(routes)}
+                {/* <Switch>
+                    <Redirect from='/' exact to='/recommend' />
+                    <Route path='/recommend' component={Recommend} />
+                    <Route path='/singer' component={Singer} />
+                    <Route path='/ranking' component={Ranking} />
+                    <Route path='*' component={Error} />
+                </Switch> */}
+            </HashRouter>
+        </Provider>
     );
 }
